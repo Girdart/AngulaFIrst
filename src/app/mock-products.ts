993ecf0d064e61c277d47140c19a675b8a4638e0
Girdart <captanrshepard@gmail.com>
2024-03-17 02:26:05 -0500

    first commit

---
+++ src/app/mock-products.ts
@@ -0,0 +1,12 @@
+import { Product } from "./product";
+
+export const Products:Product[]=[
+{name:"Samsung", price:100, description:"Phone"},
+{name:"IPhone", price:200, description:"Smartphone"},
+{name:"Asus", price:300, description:"Monitor"},
+{name:"Razor", price:400, description:"Mouse"},
+{name:"MSI", price:500, description:"Keyboard"},
+{name:"Nvidia", price:600, description:"Video Card"},
+{name:"AMD", price:700, description:"Processor"},
+{name:"Logitec", price:800, description:"Headphones"},
+]
---
+++ src/app/my-item/my-item.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Product } from '../product';
+import { Products } from '../mock-products';
+import { NgFor,NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-my-item',
+  standalone: true,
+  imports: [NgFor,NgIf],
+  templateUrl: './my-item.component.html',
+  styleUrl: './my-item.component.css'
+})
+export class MyItemComponent {
+  myProducts=Products;
+  selectedItem=Products[0];
+  onSelect(item:Product):void{this.selectedItem=item}
+}
